1fcd761eb77f6d2353702832f44b48a7c0de7572
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-09 11:24:44 +0330

    add rule engine

---
+++ src/CongestionTax.RuleEngine/RuleEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace CongestionTax.RuleEngine
+{
+    public class RuleEngine
+    {
+        List<CongestionTaxBaseRule> _rules = new();
+
+        public RuleEngine(IEnumerable<CongestionTaxBaseRule?> rules)
+        {
+            _rules.AddRange(rules);
+        }
+
+        public decimal GetCongestionTaxFee(Travel travel)
+        {
+            decimal taxFee = 0;
+            var applicapleRule = _rules.OrderBy(r => r.Proiority)
+                                        .FirstOrDefault(r => r.IsApplicable(travel));
+
+            if (applicapleRule != null) { taxFee = applicapleRule.GetTaxFee(); }
+
+            return taxFee;
+        }
+    }
+}
