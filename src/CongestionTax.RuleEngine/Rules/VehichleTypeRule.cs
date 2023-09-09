243c6ab5d02f428595df9cf672b403d2f8f16dd7
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-09 11:20:52 +0330

    add vehichle type rule

---
+++ src/CongestionTax.RuleEngine/Rules/VehichleTypeRule.cs
@@ -0,0 +1,25 @@
+﻿namespace CongestionTax.RuleEngine
+{
+    public class VehichleTypeRule : CongestionTaxBaseRule
+    {
+        public VehichleTypeRule(int proiority)
+        {
+            Proiority = proiority;
+        }
+        public override bool IsApplicable(Travel travel)
+        {
+            if (IsTaxFreeVehicle(travel.VehicleType)) return true;
+            return false;
+        }
+        public override decimal GetTaxFee()
+        {
+            return 0;
+        }
+        private bool IsTaxFreeVehicle(VehicleType vehicleType)
+        {
+            var taxFreeVehichle = Enum.IsDefined(typeof(FreeTaxVehicleType), vehicleType.ToString());
+            return taxFreeVehichle;
+        }
+
+    }
+}
---
+++ src/CongestionTax.RuleEngine/VehicleType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongestionTax.RuleEngine
+{
+    public enum VehicleType
+    {
+        Motorcycle = 0,
+        Tractor = 1,
+        Emergency = 2,
+        Diplomat = 3,
+        Foreign = 4,
+        Military = 5,
+        Others = 6
+    }
+    public enum FreeTaxVehicleType
+    {
+        Motorcycle = 0,
+        Tractor = 1,
+        Emergency = 2,
+        Diplomat = 3,
+        Foreign = 4,
+        Military = 5
+    }
+}
