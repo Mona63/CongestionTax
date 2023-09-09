26fa081142cc6493e8969a5bad8a785ea929faf7
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-09 17:04:45 +0330

    add calendar rule test

--- test/CongestionTax.Test/TaxFreeRuleTest.cs
+++ test/CongestionTax.Test/TaxFreeRuleTest.cs
@@ -13,14 +13,15 @@ namespace CongestionTax.Test
         {
 
             var rules = new List<CongestionTaxBaseRule> {
-                new VehichleTypeRule(1)
+                new VehichleTypeRule(1),
+                 new CalendarRule(2)
             };
 
             _ruleEngine = new RuleEngine.RuleEngine(rules);
         }
 
         [Test]
-        public async Task It_should_be__tax_free_for_except_vehichle()
+        public async Task It_should_be_tax_free_for_except_vehichle()
         {
             // arrange
             var travelToProcess = new Travel { Id = 1, VehicleId = 1, VehicleType = VehicleType.Motorcycle, ActionAt = DateTime.Now };
@@ -31,7 +32,18 @@ namespace CongestionTax.Test
             // assert
             taxFee.Should().Be(0);
         }
+        [Test]
+        public async Task It_should_be_tax_free_on_public_holidays()
+        {
+            // arrange
+            var travelToProcess = new Travel { Id = 1, VehicleId = 1, VehicleType = VehicleType.Others, ActionAt = new DateTime(2023,12,25) };
 
+            // act
+            var taxFee = _ruleEngine.GetCongestionTaxFee(travelToProcess);
+
+            // assert
+            taxFee.Should().Be(0);
+        }
 
     }
 }
