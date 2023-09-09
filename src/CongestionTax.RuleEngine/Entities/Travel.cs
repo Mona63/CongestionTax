ab2810ad6b599eeea3f604c423615377518b52bd
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-09 11:19:52 +0330

    add congestion tax base rule

---
+++ src/CongestionTax.RuleEngine/Entities/Travel.cs
@@ -0,0 +1,11 @@
+﻿namespace CongestionTax.RuleEngine
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+        public DateTime ActionAt { get; set; }
+        public int VehicleId { get; set; }
+        public VehicleType VehicleType { get; set; }
+
+    }
+}
