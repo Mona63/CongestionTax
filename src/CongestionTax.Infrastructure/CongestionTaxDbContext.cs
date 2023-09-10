df61cfb391e0c06b3e5d4132f4dcf704ef5d92f8
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-10 19:03:19 +0330

    add congestion tax infrastructure project

---
+++ src/CongestionTax.Infrastructure/CongestionTaxDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CongestionTax.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace CongestionTax.Infrastructure
+{
+    public class CongestionTaxDbContext : DbContext
+    {
+        public DbSet<Travel> Travels { get; set; }
+        
+        public CongestionTaxDbContext()
+        {
+        }
+        public CongestionTaxDbContext(DbContextOptions<CongestionTaxDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ src/CongestionTax.Infrastructure/Repositories-EF/TravelRepository.cs
@@ -0,0 +1,34 @@
+﻿using CongestionTax.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace CongestionTax.Infrastructure
+{
+    public class TravelRepository : ITravelRepository
+    {
+        private readonly CongestionTaxDbContext _context;
+        public TravelRepository(CongestionTaxDbContext context)
+        {
+            _context = context;
+        }
+        public Task<IEnumerable<Travel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Travel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task InsertAsync(Travel entity)
+        {
+            await _context.Travels.AddAsync(entity);
+        }
+
+        public Task UpdateAsync(Travel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
