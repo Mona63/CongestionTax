130529ea3b9d7da5b3bfe4c42a1601a58cb39b5f
Mona63 <58817509+Mona63@users.noreply.github.com>
2023-09-10 18:06:01 +0330

    add calculation toll rule test

---
+++ test/CongestionTax.Test/CalculationTollRuleTest.cs
@@ -0,0 +1,36 @@
+using CongestionTax.RuleEngine;
+using FluentAssertions;
+
+namespace CongestionTax.Test
+{
+    public class CalculationTollRuleTest
+    {
+
+
+        [Test]
+        [TestCaseSource(nameof(TimeTableChargeTest))]
+        public void It_should_be_charge_according_time_table_charge(DateTime actionAt, decimal toll)
+        {
+            // arrange
+            var travelToProcess = new Travel { Id = 1, VehicleId = 1, VehicleType = VehicleType.Others, ActionAt = actionAt };
+            var timeTableChargeRule = new TimeTableChargeRule();
+
+            // act
+            var actToll = timeTableChargeRule.CalculationToll(travelToProcess);
+
+            // assert
+            actToll.Should().Be(toll);
+        }
+        public static object[] TimeTableChargeTest =
+        {
+            new object[] { new DateTime(2013, 1, 1, 6, 0, 0), 8m },
+            new object[] { new DateTime(2013, 1, 1, 6, 30, 0), 13m },
+            new object[] { new DateTime(2013, 1, 1, 7, 0, 0), 18m },
+            new object[] { new DateTime(2013, 1, 1, 8, 0, 0), 13m },
+            new object[] { new DateTime(2013, 1, 1, 8, 30, 0), 8m },
+            new object[] { new DateTime(2013, 1, 1, 15, 0, 0), 13m },
+            new object[] { new DateTime(2013, 1, 1, 15, 30, 0), 18m },
+
+        };
+    }
+}
